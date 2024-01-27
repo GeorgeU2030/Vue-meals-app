dc37bacce91c86c23aaab400f46b1c4caea063a5
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-27 16:12:49 -0500

    config_params

--- src/views/SearchByName.vue
+++ src/views/SearchByName.vue
@@ -5,13 +5,15 @@
         @change="searchmeals"/>
     </div>
 
-    <div>
-        <div v-for="meal of meals" :key="meal.idMeal">
-            <img :src="meal.strMealThumb" :alt="meal.strMeal">
-            <h3>{{ meal.strMeal }}</h3>
-            <div>
-                <a :href="meal.strYoutube" target="_blank">YouTube</a>
-               <router-link to="/">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div v-for="meal of meals" :key="meal.idMeal" class="bg-orange-600 text-white font-semibold rounded" >
+            <router-link to="/">
+            <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded w-full h-48 object-cover">
+            </router-link>
+            <h3 class="px-3 py-2 text-xl">{{ meal.strMeal }}</h3>
+            <div class="px-3 py-2">
+                <a :href="meal.strYoutube" target="_blank" class="bg-red-600 rounded px-1 py-1">YouTube</a>
+               <router-link to="/" class="bg-amber-700 rounded px-1 py-1 ml-2">
                 View
                </router-link>
             </div>
@@ -20,11 +22,21 @@
     </template>
     
 <script setup>
-    import { ref, computed } from 'vue'
+    import { ref, computed, onMounted } from 'vue'
     import store from '../store';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
     const keyword = ref('')
     const meals = computed(() => store.state.searchedMeals)
     function searchmeals() {
         store.dispatch('searchMeals', keyword.value)
     }
+
+    onMounted(() => {
+        keyword.value= route.params.name
+        if (keyword.value){
+            searchmeals()
+        }
+    })
 </script>
